3774d90c0607ff3e89a5c2dac2779e4112e3ad92
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-06-09 12:52:27 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas
+import tkinter
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+#Label
+my_label = tkinter.Label(text="I Am a Label")
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
